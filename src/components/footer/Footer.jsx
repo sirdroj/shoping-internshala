635eb83b151586c0bdd30bea279e3e21bca09394
Aman Sharma <droj.co@gmail.com>
2023-11-03 23:07:33 +0530

    -

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,21 +4,21 @@ const Footer = ({ Pageno, setPageno,totalcards }) => {
   
   let x=Math.ceil(totalcards/15)
   return (
-    <footer className="footer_wrapper">
+    <footer className="footer_wrapper my-3">
       <div className="pagenavigator flex flex-row items-center gap-3">
         <span
-          className="pg_button"
+          className="pg_button bg-gray-300"
           onClick={() => {
             if (Pageno > 1) {
               setPageno(Pageno - 1);
             }
           }}
         >
-          previous
+          Previous
         </span>
         <div>{Pageno}/{x}</div>
         <span
-          className="pg_button"
+          className="pg_button bg-gray-300"
           onClick={() => {
             if (Pageno < x) {
               setPageno(Pageno + 1);
